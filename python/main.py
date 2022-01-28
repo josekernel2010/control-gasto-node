89f2b65b1f22ce8d9b25f67eb721d5fc7bcbc8ca
josekernel2010 <joseoca2010@hotmail.com>
2022-01-28 02:50:39 -0300

    manejo de errores

--- python/main.py
+++ python/main.py
@@ -50,22 +50,22 @@ def nuevo_movimiento():
         response2.json()
 
         tipo = input(
-            'Ingrese un número para el tipo de movimiento \n1- Ingreso\n2- Gasto\n')
+            '\nIngrese un número para el tipo de movimiento \n1- Ingreso\n2- Gasto\n')
 
         while tipo != "1" and tipo != "2":
             os.system('cls')
             tipo = input(
-                'Error ingrese un número entre 1 y 2 \n1- Ingreso\n2- Gasto\n')
+                '\nError ingrese un número entre 1 y 2 \n1- Ingreso\n2- Gasto\n')
 
         if tipo == '1':
             tipo = 'Ingreso'
         elif tipo == '2':
             tipo = 'Gasto'
 
-        cantidad = input('Ingrese la cantidad: ')
+        cantidad = input('\nIngrese la cantidad: ')
         while cantidad == '' or cantidad.isdigit() == False:
             os.system('cls')
-            cantidad = input('Error, ingrese una cantidad: ')
+            cantidad = input('\nError, ingrese una cantidad: ')
 
         fecha = solicitar_fecha()
         datos = {'tipo': tipo, 'cantidad': cantidad, 'fecha': fecha}
@@ -123,26 +123,37 @@ def buscar_movimiento():
         response2.json()
 
         # ::: problem para visualizar cuando en vez de post se pone get :v :::
-        id = input('Ingrese el id del movimiento: ')
-        response = requests.get(
-            url='http://localhost:3000/movimiento/buscar'+id)
-        datos = []
-        # convierto los datos a una lista para poder usar tabulate
-        for dato in response.json():
-            temp = []
-            for key, value in dato.items():
-                temp.append(value)
-            datos.append(temp)
-        headers = ['id', 'tipo', 'cantidad', 'fecha']
-        tabla = tabulate(datos, headers, tablefmt='fancy_grid')
-        if len(datos) > 0:
-            print(tabla)
+        id = input('\nIngrese el id del movimiento: ')
+        
+        if id == '' or id.isdigit() == False:
+            os.system('cls')
+            print("\n-----------------------------")
+            print("::Error, valor de id no valido::")
+            print("-----------------------------")
+            input("\npresione enter para seguir...")
+            os.system('cls')
         else:
-            print("\n------------------------------------")
-            print(":: El id del movimiento no existe ::")
-            print("------------------------------------")
-        input("Presione una tecla para continuar...")
-        os.system("cls")
+            response = requests.get(
+                url='http://localhost:3000/movimiento/buscar'+id)
+            
+            datos = []
+            # convierto los datos a una lista para poder usar tabulate
+            for dato in response.json():
+                temp = []
+                for key, value in dato.items():
+                    temp.append(value)
+                datos.append(temp)
+            headers = ['id', 'tipo', 'cantidad', 'fecha']
+            tabla = tabulate(datos, headers, tablefmt='fancy_grid')
+            if len(datos) > 0:
+                print(tabla)
+            else:
+                print("\n------------------------------------")
+                print(":: El id del movimiento no existe ::")
+                print("------------------------------------")
+            input("Presione una tecla para continuar...")
+            os.system("cls")
+            
     except Exception:
         print("="*25)
         print("Error en la conección a la base de datos")
@@ -158,56 +169,65 @@ def modifiacar_movimiento():
         response2 = requests.get(url='http://localhost:3000/movimiento/todos')
         response2.json()
 
-        id = input("Ingrese el id del movimiento a modificar: ")
-        existe = requests.get(
-            url='http://localhost:3000/movimiento/buscar'+id)
-
-        if len(existe.json()) > 0:
-
-            campo = input(
-                "Ingrese el campo a modificar:\n1. tipo\n2. cantidad\n3. fecha\n")
-            nuevo_valor = ''
-            if campo == '1':
-                campo = 'tipo'
-                nuevo_valor = input(
-                    "Ingrese el nuevo tipo de movimiento \n1- Ingreso\n2- Gasto\n: ")
-                while nuevo_valor != "1" and nuevo_valor != "2":
-                    os.system('cls')
-                    nuevo_valor = input(
-                        'Error ingrese un número entre 1 y 2 \n1- Ingreso\n2- Gasto\n')
-                if nuevo_valor == '1':
-                    nuevo_valor = 'Ingreso'
-                elif nuevo_valor == '2':
-                    nuevo_valor = 'Gasto'
-
-            elif campo == '2':
-                campo = 'cantidad'
-                nuevo_valor = input("Ingrese la nueva cantidad: ")
-                while nuevo_valor == '' or nuevo_valor.isdigit() == False:
-                    os.system('cls')
-                    nuevo_valor = input('Error, ingrese una cantidad: ')
-
-            elif campo == '3':
-                campo = 'fecha'
-                nuevo_valor = solicitar_fecha()
-
-            datos = {'campo': campo,
-                     'nuevo_valor': nuevo_valor}
-
-            response = requests.post(
-                url='http://localhost:3000/movimiento/modificar'+id, data=datos)
-
-            print("="*26)
-            print(response.text)
-            print("="*26)
+        id = input("\nIngrese el id del movimiento a modificar: ")
+        if id == '' or id.isdigit() == False:
+            os.system('cls')
+            print("\n-----------------------------")
+            print("::Error, valor un id valido::")
+            print("-----------------------------")
             input("\npresione enter para seguir...")
             os.system('cls')
+    
         else:
-            print("\n------------------------------------")
-            print(":: El id del movimiento no existe ::")
-            print("------------------------------------")
-            input("Presione una tecla para continuar...")
-            os.system("cls")
+            existe = requests.get(
+                url='http://localhost:3000/movimiento/buscar'+id)
+
+            if len(existe.json()) > 0:
+
+                campo = input(
+                    "Ingrese el campo a modificar:\n1. tipo\n2. cantidad\n3. fecha\n")
+                nuevo_valor = ''
+                if campo == '1':
+                    campo = 'tipo'
+                    nuevo_valor = input(
+                        "Ingrese el nuevo tipo de movimiento \n1- Ingreso\n2- Gasto\n: ")
+                    while nuevo_valor != "1" and nuevo_valor != "2":
+                        os.system('cls')
+                        nuevo_valor = input(
+                            'Error ingrese un número entre 1 y 2 \n1- Ingreso\n2- Gasto\n')
+                    if nuevo_valor == '1':
+                        nuevo_valor = 'Ingreso'
+                    elif nuevo_valor == '2':
+                        nuevo_valor = 'Gasto'
+
+                elif campo == '2':
+                    campo = 'cantidad'
+                    nuevo_valor = input("\nIngrese la nueva cantidad: ")
+                    while nuevo_valor == '' or nuevo_valor.isdigit() == False:
+                        os.system('cls')
+                        nuevo_valor = input('\nError, ingrese una cantidad: ')
+
+                elif campo == '3':
+                    campo = 'fecha'
+                    nuevo_valor = solicitar_fecha()
+
+                datos = {'campo': campo,
+                        'nuevo_valor': nuevo_valor}
+
+                response = requests.post(
+                    url='http://localhost:3000/movimiento/modificar'+id, data=datos)
+
+                print("="*26)
+                print(response.text)
+                print("="*26)
+                input("\npresione enter para seguir...")
+                os.system('cls')
+            else:
+                print("\n------------------------------------")
+                print(":: El id del movimiento no existe ::")
+                print("------------------------------------")
+                input("Presione una tecla para continuar...")
+                os.system("cls")
 
     except Exception:
         print("="*25)
@@ -224,22 +244,32 @@ def eliminar_movimiento():
         response2 = requests.get(url='http://localhost:3000/movimiento/todos')
         response2.json()
 
-        id = input("Ingrese el id del movimiento a eliminar: ")
-        existe = requests.get(
-            url='http://localhost:3000/movimiento/buscar'+id)
-
-        if len(existe.json()) > 0:
-            response = requests.post(
-                url='http://localhost:3000/movimiento/eliminar'+id)
-            print("="*35)
-            print(response.text)
-            print("="*35)
+        id = input("\nIngrese el id del movimiento a eliminar: ")
+        if id == '' or id.isdigit() == False:
+            os.system('cls')
+            print("\n-----------------------------")
+            print("::Error, valor un id valido::")
+            print("-----------------------------")
+            input("\npresione enter para seguir...")
+            os.system('cls')
+        
         else:
-            print("\n------------------------------------")
-            print(":: El id del movimiento no existe ::")
-            print("------------------------------------")
-            input("Presione una tecla para continuar...")
-            os.system("cls")
+            
+            existe = requests.get(
+                url='http://localhost:3000/movimiento/buscar'+id)
+
+            if len(existe.json()) > 0:
+                response = requests.post(
+                    url='http://localhost:3000/movimiento/eliminar'+id)
+                print("="*35)
+                print(response.text)
+                print("="*35)
+            else:
+                print("\n------------------------------------")
+                print(":: El id del movimiento no existe ::")
+                print("------------------------------------")
+                input("Presione una tecla para continuar...")
+                os.system("cls")
 
     except Exception:
         print("="*25)
